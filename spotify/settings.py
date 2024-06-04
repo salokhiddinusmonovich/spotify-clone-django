deebe5e7fbfae0d94ef6e4459c815ace76e53aef
salahYTus <usmonovsalokhiddin@gmail.com>
2024-06-04 23:35:18 +0500

    first commit

--- spotify/settings.py
+++ spotify/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%w!jj1y#e%vpqn9lh6%mwgvv+v103g_gvs(87h0!l0#y-cnzfx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
